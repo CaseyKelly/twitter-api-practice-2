9a3acbb159cf901b1c63c99c60c248fe2c8231ce
CaseyKelly <casey.kelly@colorado.edu>
2015-03-10 18:41:38 -0600

    Users can see styized main page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,15 @@
 class WelcomeController < ApplicationController
   def index
     @tweet = TweetReader.new
+    @tweets = @tweet.anyones_tweets(params[:username])
 
-
-    @othertweets = @tweet.anyones_tweets(params[:username])
     if params.has_key?(:status)
       @sendtweet = @tweet.send_tweet(params[:status])
     end
+
     if params.has_key?(:username)
       @follow_user = @tweet.follow(params[:username])
     end
+    
   end
 end
